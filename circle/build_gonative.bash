985305f822cbac01f6098910db6054ad71466755
Ondřej Kupka <ondra.cap@gmail.com>
2015-08-20 15:20:23 +0200

    wip [ci skip]

---
+++ circle/build_gonative.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+set -x
+
+PREFIX="$HOME/cache"
+
+# Exit in case the gonative directory exists already.
+if [ -d "$PREFIX/gonative" ]; then
+	echo "gonative installed already, skipping..."
+	exit 0
+fi
+
+# ---> Install the tools
+go get github.com/inconshreveable/gonative
+
+# ---> Clone Go sources
+goVersion=$(go version | awk '{ print $3 }')
+
+if [ ! -d "$PREFIX/go" ]; then
+	[ ! -d "$PREFIX" ] && mkdir -p "$PREFIX"
+	cd "$PREFIX" && git clone https://go.googlesource.com/go && cd go && git checkout "$goVersion"
+else
+	cd "$PREFIX/go" && git pull && git checkout "$goVersion"
+fi
+
+# ---> Build gonative
+mkdir -p "$PREFIX/gonative" && cd "$PREFIX/gonative"
+set +e
+GOROOT="$PREFIX/go" gonative build -src="$PREFIX/go" -platforms="windows_amd64 darwin_amd64 linux_amd64" -version="${goVersion#go}"
+if [ "$?" -ne 0 ]; then
+	rm -Rf "$PREFIX/gonative"
+	exit 1
+fi
---
+++ circle/pack.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -x
+
+SOURCES="$WORKSPACE/src/github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
+DST="$WORKSPACE/bin/dist"
+
+SALSAFLOW_VERSION="$(echo -n `"$SOURCES/salsaflow_linux_amd64" version`)"
+VERSION="$SALSAFLOW_VERSION+circleci$CIRCLE_BUILD_NUM"
+
+for os in linux windows darwin; do
+	if [ "$os" == "windows" ]; then
+		exe_suffix=".exe"
+	else
+		exe_suffix=""
+	fi
+	os_suffix="${os}_amd64"
+
+	base="salsaflow-$VERSION-${os_suffix}"
+	mkdir -p "$DST/$base"
+
+	cp "$SOURCES/salsaflow_${os_suffix}${exe_suffix}" \
+	   "$DST/$base/salsaflow${exe_suffix}"
+
+	for hook in commit-msg pre-push post-checkout; do
+		cp "$SOURCES/bin/hooks/salsaflow-${hook}/salsaflow-${hook}_${os_suffix}${exe_suffix}" \
+		   "$DST/$base/salsaflow-${hook}${exe_suffix}"
+	done
+
+	(cd "$DST" && zip -r "${base}.zip" "$base/" && cp "${base}.zip" "$CIRCLE_ARTIFACTS/")
+done
